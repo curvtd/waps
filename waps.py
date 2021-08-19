14e6466ec980a20a43981e5dfe55f6d3520f8ac5
hispect <>
2021-08-19 23:25:14 +0300

    Finished the basic functionality

--- waps.py
+++ waps.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from time import sleep
 import os
 import re
 
@@ -8,7 +9,8 @@ def get_information():
 
     while True:
         token = input("Enter API token: ")
-        verify_token = os.popen(f"curl -s https://api.telegram.org/bot{token}/getUpdates").read()
+        command = f"curl -s https://api.telegram.org/bot{token}/getUpdates"
+        verify_token = os.popen(command).read()
 
         if "true" in verify_token:
             print("Token is valid")
@@ -19,8 +21,8 @@ def get_information():
 
     while True:
         user = input("Enter your UserID: ")
-        verify_user = os.popen(f"curl -s https://api.telegram.org/bot{token}/getChat \
-                                 -d 'chat_id={user}'").read()
+        command = f"curl -s https://api.telegram.org/bot{token}/getChat -d chat_id={user}"
+        verify_user = os.popen(command).read()
 
         if "true" in verify_user:
             print("UserID is valid")
@@ -33,39 +35,38 @@ def get_information():
 
 def get_access_points():
     '''Detecting and storing access points'''
-    raw_scan = os.popen('iw dev wlan0 scan').read()
+    raw_scan = os.popen("iw dev wlan0 scan").read()
     mac = re.findall(r' (..:..:..:..:..:..)', raw_scan)     # Extracts 
     ssid = re.findall(r'SSID: (.*)', raw_scan)              # MAC,SSID,Signal
     signal = re.findall(r'signal: (-.+).00 dBm', raw_scan)  # from the command output
     access_points = {}
 
-    count = 0
-    for address in mac:
-        access_points.update({  # Сreates a nested dictionary with discovered 
-                address: {      # access points 
-                          'SSID': ssid[count],
-                          'SIGNAL': signal[count]
-                         }
-                })
-
-        count += 1
-
-    print('\nMultiple acess points detected')
+    while True:
+        try:
+            count = 0
+            for address in mac:
+                access_points.update({  # Сreates a nested dictionary with discovered 
+                        address: {      # access points 
+                                  'SSID': ssid[count],
+                                  'SIGNAL': signal[count]
+                                 }
+                        })
+                count += 1
+        except:
+            print("Something went wrong, trying again")
+            continue
+        else:
+            print('\nMultiple access points detected')
+            break
     return access_points
 
 
 def get_allowed_mac():
     '''Obtaining allowed MAC addresses'''
-    try:
-        with open('./whitelist.txt') as file:
-            whitelist = file.read().splitlines()
-        if whitelist == '':
-            raise NoAllowedAP
-    except:
-        print('Please, write the allowed MAC address in whitelist.txt\n'+\
-              'One MAC address per line')
-    else:
-        return whitelist
+    with open('./whitelist.txt') as file:
+        whitelist = file.read().lower().splitlines()
+
+    return whitelist
 
 
 def delete_allowed():
@@ -81,3 +82,27 @@ def delete_allowed():
     return access_points
 
 
+def create_report():
+    '''Creating a report and sending it to Telegram'''
+    name, token, user = get_information()
+    time_amount = int(input("Enter the number of seconds to sleep: "))
+
+    while True:
+        access_points = delete_allowed()
+
+        if access_points:
+            print("Creating a report")
+            message = f"{name} - Detected unknown access points\n(MAC, SIGNAL, SSID)"
+            for mac, ap_info in access_points.items():
+                message += f"\n{mac}    {ap_info['SIGNAL']}    {ap_info['SSID']}"
+
+            print("Sending the report to Telegram")
+            command = f"curl -s https://api.telegram.org/bot{token}/sendMessage -d chat_id={user} -d text='{message}'"
+            os.system(command)
+        else:
+            print('No unknown access points detected')
+
+        sleep(time_amount)
+
+
+create_report()
